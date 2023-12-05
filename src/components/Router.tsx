ff472920ce1410ca5dca1a06f61f51dc45b605d7
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-05 18:15:02 -0500

    created router and route file

---
+++ src/components/Router.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const AppRouter = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={} />
+            </Switch>
+        </Router>
+    )
+}
+
+export default AppRouter;
