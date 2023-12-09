4f10151744d5fa9b7fd7be2f6135cedca024f37d
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-09 16:21:16 -0500

    added routerprops

--- src/types/component-types.ts
+++ src/types/component-types.ts
@@ -1,9 +1,9 @@
-export interface RoutePathProps {
-key: number;
+export interface RouteConfig {
+  path: string;
+  component: React.ComponentType;
+  children?: RouteConfig[];
 }
 
-export interface RouteConfig {
-    path: string;
-    component: React.ComponentType;
-    children?: RouteConfig[];
-  }
+export interface RouterProps {
+  routes: RouteConfig[];
+}
