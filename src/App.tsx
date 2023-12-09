fa2cc51e800c001c0eb8625014883b93d65aaf81
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-09 15:19:45 -0500

    cleaned up code

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  return ( 
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  </BrowserRouter>
-  )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
