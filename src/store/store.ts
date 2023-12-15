3afcd91b28efd26165d30d1f935beba78e0298c0
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-14 19:19:02 -0500

    const store

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,3 +4,7 @@ import userReducer from "../reducers/userReducer";
 const rootReducer = combineReducers({
   user: userReducer,
 });
+
+const store = createStore(rootReducer);
+
+export default store;
