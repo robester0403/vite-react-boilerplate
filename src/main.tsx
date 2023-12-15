3ae2a28f37b2ee883efc2b48024626d69b740ff5
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-14 19:23:16 -0500

    added provider and passed in store and routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import store from "./store/store.ts";
+import "./index.css";
+import { routes } from "./router/routes.ts";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App routes={routes} />
+    </Provider>
+  </React.StrictMode>
+);
