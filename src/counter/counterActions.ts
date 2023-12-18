07010c3bc32be715625c58555cfaa27efe7daaa3
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-18 17:56:14 -0500

    created incrementasync

---
+++ src/counter/counterActions.ts
@@ -0,0 +1,9 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const incrementAsync = createAsyncThunk(
+  "counter/incrementAsync",
+  async (amount: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return amount;
+  }
+);
