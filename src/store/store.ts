f74bb4aab75b5dd27cd28e7b101fdd794990005e
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-18 16:34:54 -0500

    export type rootstate and appdispatch

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,4 +3,6 @@ import { configureStore } from "@reduxjs/toolkit";
 const store = configureStore({
   reducer: rootReducer,
 });
+
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
