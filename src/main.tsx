d5c75d94e7d9ef6ef811d011b102b664d6ec47a2
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-18 18:04:23 -0500

    fixed import for store

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
-import store from "./store/store.ts";
+import { store } from "./store/store.ts";
 import "./index.css";
 import { routes } from "./router/routes.ts";
 
