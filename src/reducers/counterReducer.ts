494efe9bbb51066195fd9731a82f19cbfb34ac59
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-18 17:49:38 -0500

    updating state with decrement

--- src/reducers/counterReducer.ts
+++ src/reducers/counterReducer.ts
@@ -4,3 +4,16 @@ import { CounterState } from "../types/redux-types";
 const initialState: CounterState = {
   value: 0,
 };
+
+const counterReducer = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+});
