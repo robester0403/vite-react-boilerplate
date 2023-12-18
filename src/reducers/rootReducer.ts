0701b9d1407e30a85e5c2bb26bde59271e74c26d
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-18 16:37:19 -0500

    const initialState and passed in CounterState

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,1 +1,6 @@
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { CounterState } from "../types/redux-types";
+
+const initialState: CounterState = {
+  value: 0,
+};
