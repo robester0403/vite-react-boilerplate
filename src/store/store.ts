e5dff714bc70e0e6c10a53cbfd9a14f5bdf81d33
Felix Lai <91552420+felixlaii@users.noreply.github.com>
2023-12-22 11:17:00 -0500

    removed counter

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../counter/counterReducer";
 import rootReducer from "../reducers/rootReducer";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     user: rootReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+3;
